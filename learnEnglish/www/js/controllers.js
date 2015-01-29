bf86b563b4fc711590c6b7d8fa366bc8f97e67dd
TNMTri <tri.tran@softersoucre.com>
2015-01-29 17:41:19 +0700

    Backup.

--- learnEnglish/www/js/controllers.js
+++ learnEnglish/www/js/controllers.js
@@ -1,48 +1,246 @@
-angular.module('starter.controllers', [])
+var db = null;
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+angular.module('starter.controllers', ['ngCordova', 'ionic'])
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    //menu
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $ionicPopup, $stateParams, $state, $cordovaSQLite) {
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        $scope.topics = [];
+        $scope.data = {};
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        //Create modal (popup)
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        //Open new topic modal
+        $scope.show_new_topic = function () {
+            $scope.modal.show();
+        };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+        //Close new topic modal
+        $scope.close_new_topic = function () {
+            $scope.modal.hide();
+        };
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+        //Insert new topic.
+        $scope.insert_topic = function () {
+            alert("insert nè");
+            var query = "INSERT INTO topic (topic_name) VALUES (?)";
+            $cordovaSQLite.execute(db, query, [$scope.data.topicName]).then(function (res) {
+                alert("INSERT ID -> " + res.insertId);
+                $scope.close_new_topic();
+            }, function (err) {
+                console.error(err);
+            });
+        };
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+        /*$scope.showPopup = function () {
+         $scope.data = {};
+         // An elaborate, custom popup
+         var myPopup = $ionicPopup.show({
+         title: 'Rename Topic',
+         subTitle: 'Please enter topic name',
+         scope: $scope,
+         buttons: [
+         { text: 'Cancel' },
+         {
+         text: '<b>Save</b>',
+         type: 'button-positive',
+         onTap: function (e) {
+         if (!$scope.data.topic_new_name) {
+         e.preventDefault();
+         } else {
+         return $scope.data.topic_new_name;
+         }
+         }
+         }
+         ]
+         });
+         myPopup.then(function (res) {
+         console.log($scope.topics);
+         $scope.topics.forEach(function (topic) {
+         if (topic.id.equal($stateParams.id)) {
+         console.log("có nha, có nha");
+         }
+         //console.log('Tapped!', res);
+         })
+         });
+         };*/
+    })
+
+    .controller('PlaylistsCtrl', function ($scope, $ionicModal, $cordovaSQLite, $rootScope) {
+        //Create modal (popup)
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        //Open new topic modal
+        $scope.show_new_topic = function () {
+            $scope.modal.show();
+        };
+
+        //Close new topic modal
+        $scope.close_new_topic = function () {
+            $scope.modal.hide();
+        };
+
+        //Insert new topic.
+        $scope.insert_topic = function () {
+            alert("insert nè");
+            var query = "INSERT INTO topic (topic_name) VALUES (?)";
+            $cordovaSQLite.execute(db, query, [$scope.data.topic_name]).then(function (res) {
+                alert("INSERT ID -> " + res.insertId);
+                $scope.modal.hide();
+                $scope.topics.push({id: res.insertId, topic_name: $scope.data.topic_name});
+            }, function (err) {
+                console.error(err);
+            });
+        };
+    })
+
+    .controller('PlaylistCtrl', function ($scope, $stateParams, $state, $cordovaSQLite) {
+
+        /*console.log("ID cua topic: " + $stateParams.id);
+         var id_topic = $stateParams.id_topic;*/
+
+        $scope.items = [];
+        /*$scope.topics.forEach(function (topic) {
+         if (topic.id == $stateParams.id_topic) {
+         $scope.topic.push(topic);
+         }
+         });*/
+
+        $scope.edit_topic = function () {
+            alert("edit name topic :D");
+        };
+
+        $scope.delete_topic = function () {
+            $scope.topics.forEach(function (topic) {
+                if (topic.id == $stateParams.id) {
+                    var x = $scope.topics.indexOf($stateParams.id);
+                    console.log("param " + $stateParams.id);
+                    $scope.topics.splice(x, 1);
+                    $state.go('app.playlists');
+                }
+                //new delete
+                var query = "DELETE FROM topic WHERE ID = ?";
+                $cordovaSQLite.execute(db, query, [$stateParams.id]).then(function (res) {
+                    alert("delete success");
+                    $state.go('app.playlists');
+                }, function (err) {
+                    alert(err);
+                });
+            });
+        };
+
+        var id_topic = $stateParams.id_topic;
+
+        $scope.play_topic = function () {
+            var query = "SELECT * FROM item WHERE id_topic = " + id_topic;
+            $cordovaSQLite.execute(db, query).then(function (res) {
+                if (res.rows.length > 0) {
+                    //console.log("SELECTED -> " + res.rows.item(0).topic_name);
+                    $scope.items = [];
+                    for (var i = 0; i < res.rows.length; i++) {
+                        //alert(res.rows.item(i).topic_name);
+                        $scope.items.push({id: res.rows.item(i).id, content: res.rows.item(i).content});
+                    }
+                } else {
+                    alert("No results found, please insert.");
+                }
+            }, function (err) {
+                console.error(err);
+            });
+        };
+
+        //insert new item in this list
+        $scope.new_item = function () {
+            /*alert($scope.data.topicName);*/
+            var query = "INSERT INTO item (id_topic, content) VALUES (?, ?)";
+            $cordovaSQLite.execute(db, query, [$stateParams.id_topic, $scope.data.topicName]).then(function (res) {
+                alert("INSERT ID -> " + res.insertId);
+            }, function (err) {
+                console.error(err);
+            });
+        };
+
+        //delete item in topic0
+        $scope.delete_item = function (id_item) {
+            var query = "DELETE FROM item WHERE id = " + id_item;
+            $cordovaSQLite.execute(db, query).then(function (res) {
+                alert("INSERT ID -> " + res.insertId);
+            }, function (err) {
+                console.error(err);
+            });
+        };
+    })
+
+    .controller('BrowseCtrl', function ($scope, $cordovaSQLite) {
+        $scope.insert_topic_browse = function () {
+            console.log("Insert nha!!!");
+            alert($scope.data.topicName);
+            var query = "INSERT INTO topic (topic_name) VALUES (?)";
+            $cordovaSQLite.execute(db, query, [$scope.data.topicName]).then(function (res) {
+                alert("INSERT ID -> " + res.insertId);
+            }, function (err) {
+                console.error(err);
+            });
+        };
+
+        $scope.select_topic_browse = function () {
+            console.log("Select nha nha!!!");
+            var query = "SELECT * FROM topic";
+            $cordovaSQLite.execute(db, query).then(function (res) {
+                if (res.rows.length > 0) {
+                    console.log("SELECTED -> " + res.rows.item(0).topic_name);
+                    $scope.topics = [];
+                    for (var i = 0; i < res.rows.length; i++) {
+                        //alert(res.rows.item(i).topic_name);
+                        $scope.topics.push({id: res.rows.item(i).id, topic_name: res.rows.item(i).topic_name})
+                    }
+                } else {
+                    console.log("No results found");
+                }
+            }, function (err) {
+                console.error(err);
+            });
+        };
+
+        $scope.delete_topic_browse = function () {
+            alert("vô");
+            db = $cordovaSQLite.deleteDB("my.db");
+            $cordovaSQLite.deleteDB("my.db");
+        }
+    })
+
+    .controller('PlayCtrl', function ($scope, $stateParams, $cordovaSQLite) {
+
+        var id_topic = $stateParams.id_topic;
+
+        $scope.play_topic = function () {
+            var query = "SELECT * FROM item WHERE id_topic = " + id_topic;
+            $cordovaSQLite.execute(db, query).then(function (res) {
+                if (res.rows.length > 0) {
+                    //console.log("SELECTED -> " + res.rows.item(0).topic_name);
+                    $scope.items = [];
+                    for (var i = 0; i < res.rows.length; i++) {
+                        //alert(res.rows.item(i).topic_name);
+                        $scope.items.push({id: res.rows.item(i).id, content: res.rows.item(i).content});
+                    }
+                } else {
+                    alert("No results found, please insert.");
+                }
+            }, function (err) {
+                console.error(err);
+            });
+        };
+
+        $scope.stop_topic = function () {
+            //stop, back to playlist with id
+        }
+    });
